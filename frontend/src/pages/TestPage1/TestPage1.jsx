067681939dd1801fbfa99789203ce55cf2cd4418
pranulkbv28 <28bvpranaav@gmail.com>
2024-11-01 22:35:47 +0530

    basic structure and routing provided v4

--- frontend/src/pages/TestPage1/TestPage1.jsx
+++
@@ -1,5 +0,0 @@
-function TestPage1() {
-  return <div>TestPage1</div>;
-}
-
-export default TestPage1;
--- frontend/src/pages/TestPage2/TestPage2.jsx
+++
@@ -1,5 +0,0 @@
-function TestPage2() {
-  return <div>TestPage2</div>;
-}
-
-export default TestPage2;
